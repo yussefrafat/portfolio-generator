b09d6f093b9c282a629b3c6be0a044b1d9783f8f
Yussef Rafat <yussefrafat5908@gmail.com>
2021-07-17 17:32:37 -0400

    template

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const generatePage = require('./page-template');
+
 const inquirer = require('inquirer');
 
 inquirer
@@ -133,6 +136,13 @@ inquirer
   promptUser()
     .then(promptProject)
     .then(portfolioData => {
-      console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
     });
+    });
+
 
